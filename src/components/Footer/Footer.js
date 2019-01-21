6388bce131f9bf66313928cf6d0fdca5aec48d01
ewf-updater <ewf-updater@ewf.org>
2019-01-21 15:05:25 +0100

    changed date in footer to 2019

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,7 +16,7 @@ class Footer extends Component {
     return (
       <div className="fix-footer">
             <div className="footer-text">
-                <p className="has-text-centered"><a href="https://slock.it/impressum.html">Legal Notice</a> - <a href="https://slock.it/privacy_web.html">Privacy Notice</a> - © Copright 2015-2018 Slock.it GmbH - All Rights Reserved</p>
+                <p className="has-text-centered"><a href="https://slock.it/impressum.html">Legal Notice</a> - <a href="https://slock.it/privacy_web.html">Privacy Notice</a> - © Copright 2015-2019 Slock.it GmbH - All Rights Reserved</p>
             </div>
       </div>
       );
